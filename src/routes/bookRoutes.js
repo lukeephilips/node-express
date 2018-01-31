ba50c636be260c110e926006c6e912f3487cb3d8
Luke <luke.e.philips@gmail.com>
2018-01-30 17:05:45 -0800

    back in business

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -10,37 +10,36 @@ var colors = require('colors');
 var router = (nav, client) => {
   bookRouter.route('/')
     .get((req, res) => {
-      // var dbValues = client.query(`SELECT * FROM books`, (error, result) => {
-      //   res.render('books', {
-      //     title: 'all the books',
-      //     nav,
-      //     books: result.rows,
-      //   });
-      // });
-      res.send('ding dong');
+      var dbValues = client.query(`SELECT * FROM books`, (error, result) => {
+        res.render('books', {
+          title: 'all the books',
+          nav,
+          books: result.rows,
+        });
+      });
     });
 
-  // bookRouter.route('/:id')
-  //   .all(function(req, res, next) {
-  //     var id = req.params.id;
-  //
-  //     var dbValues = client.query(`SELECT * FROM books
-  //       WHERE id= ${id}`, (error, result) => {
-  //       if (result.rows.length === 0) {
-  //         res.status(404).send('Book not found');
-  //       } else {
-  //         req.book = result.rows[0];
-  //         next();
-  //       }
-  //     });
-  //   })
-  //   .get((req, res) => {
-  //     res.render('book', {
-  //       nav,
-  //       book: req.book,
-  //     });
-  //     return bookRouter;
-  //   });
+  bookRouter.route('/:id')
+    .all(function(req, res, next) {
+      var id = req.params.id;
+
+      var dbValues = client.query(`SELECT * FROM books
+        WHERE id= ${id}`, (error, result) => {
+        if (result.rows.length === 0) {
+          res.status(404).send('Book not found');
+        } else {
+          req.book = result.rows[0];
+          next();
+        }
+      });
+    })
+    .get((req, res) => {
+      res.render('book', {
+        nav,
+        book: req.book,
+      });
+    });
+  return bookRouter;
 };
 
 module.exports = router;
